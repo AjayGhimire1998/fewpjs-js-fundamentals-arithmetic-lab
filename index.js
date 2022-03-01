bffb240fe8ae1f583e2f7743b4c6c8312c8015f5
AjayGhimire <ajayghimire1998@gmail.com>
2022-03-01 14:53:20 +1100

    done with the lab

--- index.js
+++ index.js
@@ -1,2 +1,74 @@
 
 // Write your code here
+{const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+console.log(multiply);
+
+}
+
+// function random(min, max){
+//     min = Math.ceil(min);
+//     max = Math.floor(max);
+//     return Math.random();
+// 
+ 
+
+const min = 5;
+const maxi = 10;
+const random = Math.floor(Math.random() * (maxi-min));
+
+console.log(random);
+
+// let numb1 = 16;
+// let numb2 = 6;
+// const mod = numb1 % numb2;
+
+// console.log(mod);
+// function max(maxi){
+// const maxi = Math.max(5, 10, 15, 20);
+
+// return maxi;
+// }
+
+// max = findMax (1, 5, 15, 20, 17);
+// function findMax(){
+//     let i;
+//     let max = -Infinity;
+//     for (i = 0; i < arguments.length; i++){
+//         if (arguments[i] > max){
+//             max = arguments[i];
+//         }
+//     }
+//     return max;
+
+const num3 = 19;
+const num4 = 5;
+
+const mod = num3 % num4;
+console.log (mod);
+
+
+
+
+// let set = (1, 5, 15, 20);
+// let findMax = Math.max(set);
+// console.log( findMax);
+
+let max = Math.max(1, 5,15,20);
+
+
+
+
+
+
+    
+
+
+
+
+               
+    
+
+
